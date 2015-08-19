33f6b6832fe73d1dc0eca5d45c817082394cdcbe
Zhongqi Sun <zhongqi.sun@gmail.com>
2015-08-19 13:10:30 -0700

    set base template path

--- buybuybiz_ex/settings.py
+++ buybuybiz_ex/settings.py
@@ -55,10 +55,12 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'buybuybiz_ex.urls'
 
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_PATH,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
