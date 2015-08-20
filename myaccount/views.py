83fd439c06eccad541767a36cac9702e0751f0c6
Zhongqi Sun <zhongqi.sun@gmail.com>
2015-08-19 21:26:27 -0700

    Add index view for myaccount

--- myaccount/views.py
+++ myaccount/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse,HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib import messages
 
 # Create your views here.
+@login_required
+def index(request):
+	projectsCount = bizProject.objects.filter(sold_by=request.user).count()
+	context = {'projectsCount': projectsCount}
+	return render(request, 'myaccount/index.html', context)
