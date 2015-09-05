c4117789e42307fe5eeec76533138c14ee8b6005
Zhongqi Sun <zhongqi.sun@gmail.com>
2015-09-05 10:18:26 -0700

    Add "Goal" to forms and templates

--- myaccount/forms.py
+++ myaccount/forms.py
@@ -4,7 +4,7 @@ from myaccount.models import bizProject,projectPic
 class ProjectForm(ModelForm):
 	class Meta:
 		model = bizProject
-		fields = ['name','description','price','category']
+		fields = ['name','description','price','category','goal']
 
 class projectPicform(ModelForm):
 	class Meta:
