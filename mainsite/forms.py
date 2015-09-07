b9fdbb0ac24b8b37ef78326fb590a3dc9441bc65
Zhongqi Sun <zhongqi.sun@gmail.com>
2015-09-07 16:30:34 -0700

    Add mainsite app

---
+++ mainsite/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from haystack.forms import SearchForm
+from myaccount.models import bizCategory, bizGoal
+
+class ProjectSearchForm(SearchForm):
+	category = forms.ModelChoiceField(queryset=bizCategory.objects.all(),required=False,empty_label="All Categories")
+	goal = forms.ModelChoiceField(queryset=bizGoal.objects.all(),required=False,empty_label="All Goals")
+	def serch(self):
+		sqs = super(ProjectSearchForm, self).search()
+		if not self.is_valid():
+			return self.no_query_found()
+
+		if self.cleaned_data['category']:
+			sqs = sqs.filter(category=self.cleaned_data['category'])
+		if self.cleaned_data['goal']:
+			sqs = sqs.filter(goal=self.cleaned_data['goal'])
+		return sqs
+	
