0563f91aed2b4e0bc41d99914a6bff12402d0c05
Zhongqi Sun <zhongqi.sun@gmail.com>
2015-09-17 15:32:17 -0700

    Add unicode return value for Partners model

--- mainsite/models.py
+++ mainsite/models.py
@@ -7,3 +7,5 @@ class Partners(models.Model):
 	title = models.CharField(max_length=100)
 	description = models.TextField(default=" ")
 	picture = models.ImageField(upload_to='partner_images',blank=True)
+	def __unicode__(self):
+		return self.name
