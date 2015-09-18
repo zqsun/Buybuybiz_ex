101ef2827ef66a1f13d8a26621267413bd3ef473
Zhongqi Sun <zhongqi.sun@gmail.com>
2015-09-17 17:50:49 -0700

    Change the empty label of goal from "all goals" to "US israel tech"

--- mainsite/forms.py
+++ mainsite/forms.py
@@ -5,7 +5,7 @@ from mainsite.models import Partners
 
 class ProjectSearchForm(SearchForm):
 	category = forms.ModelChoiceField(queryset=bizCategory.objects.all(),required=False,empty_label="All Categories")
-	goal = forms.ModelChoiceField(queryset=bizGoal.objects.all(),required=False,empty_label="All Goals")
+	goal = forms.ModelChoiceField(queryset=bizGoal.objects.all(),required=False,empty_label="U.S./ Israel Technologies")
 	def serch(self):
 		sqs = super(ProjectSearchForm, self).search()
 		if not self.is_valid():
